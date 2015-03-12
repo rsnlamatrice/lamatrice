b6207f7fdefa3f9423fa43702725967b17ee8aef
rsnlamatrice <edid@free.fr>
2015-03-12 18:28:30 +0000

    Install module ContactAddresses

---
+++ languages/fr_fr/ContactAddresses.php
@@ -0,0 +1,22 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$languageStrings = array(
+	'ContactAddresses' => 'Adresses des contacts',
+	'SINGLE_ContactAddresses' => 'Adresse de contact',
+	'LBL_BLOCK_GENERAL_INFORMATION' => 'Informations générales',
+	'LBL_BLOCK_SYSTEM_INFORMATION' => 'Informations du système',
+	'LBL_ADDRESSTYPE' => 'Type',
+	'LBL_CONTACTID' => 'Contact',
+	'LBL_COMMENTS' => 'Commentaires',
+
+);
+
+?>
