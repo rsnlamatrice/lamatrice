f9c15bc018f58e38b7a10116603f7413821e0efa
rsnlamatrice <edid@free.fr>
2015-08-31 12:55:45 +0200

    pull

--- modules/RSNImportSources/views/ImportFromDB.php
+++ modules/RSNImportSources/views/ImportFromDB.php
@@ -1,6 +1,6 @@
 <?php
 
-define('MAX_QUERY_ROWS', 5000); //DEBUG
+define('MAX_QUERY_ROWS', 2500); //DEBUG
 
 class RSNImportSources_ImportFromDB_View extends RSNImportSources_ImportFromFile_View {
 
