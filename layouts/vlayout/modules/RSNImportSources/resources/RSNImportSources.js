498530dd43426710417564f485df682c028fcd94
edvariables <admin@edvariables.net>
2015-09-29 14:48:56 +0200

    Import Petitions

--- layouts/vlayout/modules/RSNImportSources/resources/RSNImportSources.js
+++ layouts/vlayout/modules/RSNImportSources/resources/RSNImportSources.js
@@ -14,29 +14,71 @@ if (typeof(RSNImportSourcesJs) == 'undefined') {
 		 * Get More Preview Data
 		 */
     	registerGetMorePreviewDataEvent: function() {
+			var thisInstance = this;
 			jQuery('body').on('click', '.getMorePreviewData', function(e){
 				e.preventDefault();
 				
-				var url = this.href
-				, $thisElement = $(this)
-				, $destTablePage = $thisElement.parents('table:first')
-				, $destTableData = $destTablePage.find('table.importContents:first');
-				AppConnector.request(url).then(
-					function(data){
-						var $data = $(data)
-						, $trs = $data.find('table.importContents > tbody > tr')
-						, $tfootRows = $data.find('table.searchUIBasic > tfoot > tr')
-						;
-						$destTableData.children('tbody').append($trs);//append rows
-						$destTablePage.children('tfoot').html($tfootRows);//replace table foot for next link
-						
-					},
-					function(error){
-					}
-				);
+				thisInstance.getMorePreviewData(e);
 				
 				return false;
 			});
+		},
+		
+		/**
+		 * TODO pas sûr que ça fonctionne avec plusieurs modules simultannéement
+		 */
+		reloadPreviewData : function(e){
+			var $thisElement = $(e.target)
+			, $destTablePage = $thisElement.parents('table.importPreview:first')
+			, $destTableData = $destTablePage.find('table.importContents:first')
+			, url = $destTableData.find('param[name="PREVIEW_DATA_URL"]').attr('value')
+			, params = $.param(this.getPreviewDataRequestParams(e));
+			AppConnector.request(url + '&' + params).then(
+				function(data){
+					var $data = $(data)
+					, $trs = $data.find('table.importContents > tbody > tr')
+					, $tfootRows = $data.find('table.searchUIBasic > tfoot > tr')
+					;
+					$destTableData.children('tbody').html($trs);//replace rows
+					$destTablePage.children('tfoot').html($tfootRows);//replace table foot for next link
+					
+				},
+				function(error){
+				}
+			);
+		},
+		
+		getMorePreviewData : function(e){
+			var url = e.target.href
+			, $thisElement = $(e.target)
+			, $destTablePage = $thisElement.parents('table.importPreview:first')
+			, $destTableData = $destTablePage.find('table.importContents:first')
+			, params = $.param(this.getPreviewDataRequestParams(e));
+			AppConnector.request(url + '&' + params).then(
+				function(data){
+					var $data = $(data)
+					, $trs = $data.find('table.importContents > tbody > tr')
+					, $tfootRows = $data.find('table.searchUIBasic > tfoot > tr')
+					;
+					$destTableData.children('tbody').append($trs);//append rows
+					$destTablePage.children('tfoot').html($tfootRows);//replace table foot for next link
+					
+				},
+				function(error){
+				}
+			);
+		},
+		
+		getPreviewDataRequestParams : function(e){
+			var $thisElement = $(e.target)
+			, $destTablePage = $thisElement.parents('table.importPreview:first')
+			, $destTableData = $destTablePage.find('table.importContents:first')
+			, $filters = $destTableData.find('.header-filters :input[name]')
+			, params = {};
+			$filters.each(function(){
+				params[this.name] = this.value;
+			});
+			return params;
 		},
 
     	registerSelectSourceEvent: function() {
