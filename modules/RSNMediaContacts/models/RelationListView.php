76205f08a9b6b94e367807dae4aa4ca502a3f780
rsnlamatrice <edid@free.fr>
2015-10-13 16:24:33 +0200

    Suppr Import Relation Medias

---
+++ modules/RSNMediaContacts/models/RelationListView.php
@@ -0,0 +1,13 @@
+<?php
+/*+***********************************************************************************
+ * 
+ *************************************************************************************/
+
+class RSNMediaContacts_RelationListView_Model extends Vtiger_RelationListView_Model {
+
+	/* ED150814 */
+	public function getImportRelationLinks() {
+		return array();
+	}
+
+}
