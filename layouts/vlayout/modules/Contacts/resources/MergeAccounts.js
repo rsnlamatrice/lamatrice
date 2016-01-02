f460ba1f059cdf5427b363dc1eed14bb72865459
edvariables <admin@edvariables.net>
2016-01-02 09:57:40 +0100

    Merge accounts et contacts

---
+++ layouts/vlayout/modules/Contacts/resources/MergeAccounts.js
@@ -0,0 +1,9 @@
+/*+***********************************************************************************
+ * Inheritance of Vtiger_MergeRecord_Js
+ * In MergeAccounts.tpl, see <input id="popUpClassName" type="hidden" value="Contacts_MergeAccounts_Js"/>
+ *************************************************************************************/
+Vtiger_MergeRecord_Js("Contacts_MergeAccounts_Js",{
+
+},{
+
+});
