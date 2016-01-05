2d898e04d1509fe8e35242f37e99ab6ba023a36d
VERNEREY <aurelien.vernerey@sortirdunucleaire.fr>
2016-01-05 17:12:55 +0100

    add the export for the revue

--- modules/Contacts/exports/ExportRevue.php
+++ modules/Contacts/exports/ExportRevue.php
@@ -5,21 +5,25 @@
 
 class Contacts_ExportRevue_Export extends Export_ExportData_Action {
 
+	//tmp check mailing ou other address !!!!!!!
 	function getExportStructure() {
 		return array(
-			"Contact ID" => "contact_no",
-			"Empty" => "",
+			"Code client" => "contact_no",
+			"Civilité" => "",//tmp ??
 			"Nom" => "lastname",
 			"Prenom" => "firstname",
-			"structure name" => "",
-			"adresse 1" => "mailingstreet3",
-			"adresse 2" => "mailingstreet",
-			"adresse 3" => "mailingstreet2",
-			"zipcode" => "mailingzip",
-			"ville" => "mailingcity",
-			"pays" => "mailingcountry",
-			"message personalisé" => function($row) { return "Merci de vous réabonner (bulletin en p.9)"; },//tmp check witch message must be displayed ...
-			"bonjour" => function ($row) { return "Bonjour " . $row["firstname"] . ","; }//tmp check if structure or person ...
+			"Société" => function ($row) { return Contacts_ExportRevue_Export::getAddressField($row, "street2"); },
+			"adresse 1" => function ($row) { return Contacts_ExportRevue_Export::getAddressField($row, "street3"); },
+			"adresse 2" => function ($row) { return Contacts_ExportRevue_Export::getAddressField($row, "street"); },
+			"adresse 3" => function ($row) { return Contacts_ExportRevue_Export::getPoBox($row); },
+			"Code Postal" => function ($row) { return Contacts_ExportRevue_Export::getZipCode($row); },
+			"Ville (ou Pays)" => function ($row) { return Contacts_ExportRevue_Export::getCityOrCountry($row); },
+			"Nb exemplaires" => "",//tmp
+			"Mode envoi" => "",//tmp to check ??
+			"info lib 1" => function ($row) { return Contacts_ExportRevue_Export::getMessage1($row); },
+			"info lib 2" => function ($row) { return Contacts_ExportRevue_Export::getMessage2($row); },
+			"info lib 3" => function ($row) { return Contacts_ExportRevue_Export::getMessage3($row); },
+			"info lib 4" => function ($row) { return Contacts_ExportRevue_Export::getMessage4($row); }
 		);
 	}
 	
@@ -34,5 +38,52 @@ class Contacts_ExportRevue_Export extends Export_ExportData_Action {
 
 	function getQueryOrderBy($moduleName) {
 		return ' ORDER BY mailingcountry ASC, mailingzip ASC';
+	}
+
+	function getAddressToUse($row) {
+		if ($row["use_address2_for_revue"]) {
+			return "other";
+		}
+
+		return "mailing";
+	}
+
+	function getAddressField($row, $field) {
+		return $row[$this->getAddressToUse($row) . $field];
+	}
+
+	function isForeign($row) {
+		$country = $this->getAddressField($row, "country");
+		return $country != "" && strtoupper($country) != "FRANCE";
+	}
+
+	function getPoBox($row) {
+		//tmp display pobox from foreign mailing ??
+		return ($this->isForeign($row)) ? $this->getAddressField($row, "pobox") . " " . $this->getAddressField($row, "zip") . " " . $this->getAddressField($row, "city")
+			: $this->getAddressField($row, "pobox");
+	}
+
+	function getZipCode($row) {
+		return ($this->isForeign($row)) ? "" : $this->getAddressField($row, "zip");
+	}
+
+	function getCityOrCountry($row) {
+		return ($this->isForeign($row)) ? $this->getAddressField($row, "country") : $this->getAddressField($row, "city");
+	}
+
+	function getMessage1($row) {
+		return "Merci de vous réabonner (bulletin en p.9)";//tmp check witch message must be displayed ...
+	}
+
+	function getMessage2($row) {
+		return "Bonjour " . $row["firstname"] . ",";//tmp check if structure or persone ...
+	}
+
+	function getMessage3($row) {
+		return "";
+	}
+
+	function getMessage4($row) {
+		return "";
 	}
 }
