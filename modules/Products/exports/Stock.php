7360502f5904fb99d5a1c2073cea35fd1dea6ca9
VERNEREY <aurelien.vernerey@sortirdunucleaire.fr>
2016-01-11 11:55:18 +0100

    add Product export for Rene

---
+++ modules/Products/exports/Stock.php
@@ -0,0 +1,26 @@
+<?php
+/*+***********************************************************************************
+	AV1511
+ *************************************************************************************/
+
+//code produit; désignation; prix d'achat; quantité en stock; quantité en dépôt vente.
+class Products_Stock_Export extends Products_ProductsAll_Export {
+	function getExportStructure() {
+		return array(
+			"Part Number" => "productcode",
+			"Product Name" => "productname",
+			"Purchase price" => function($row) { return Products_ProductsAll_Export::formatFloatVal($row["purchaseprice"]); },
+			"Qty In Stock" => function($row) { return intval($row["qtyinstock"]); },
+			"Qty In Demand" => "qtyindemand",
+		);
+	}
+	
+	function displayHeaderLine() {
+		return true;
+	}
+
+	function getExportFileName($request) {
+		$moduleName = $request->get('source_module');
+		return "export_produits_pour_stock";
+	}
+}
