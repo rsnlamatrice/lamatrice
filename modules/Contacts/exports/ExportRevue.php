a4753477bd962cbd6db71afbf240885703074037
VERNEREY <aurelien.vernerey@sortirdunucleaire.fr>
2016-01-18 17:08:30 +0100

    is abo not correct for those how does not have finabo

--- modules/Contacts/exports/ExportRevue.php
+++ modules/Contacts/exports/ExportRevue.php
@@ -87,7 +87,7 @@ class Contacts_ExportRevue_Export extends Export_ExportData_Action {
 		$today = time() - 31 * 24 * 60 * 60;//aujourd'hui - 1 mois
 		$finabo = strtotime($row["finabo"]);
 		//echo "today $today -> finabo : $finabo<br/>";
-		return $finabo >= $today;//$finabo >= $today;
+		return ($finabo >= $today) || !$finabo;//$finabo >= $today;
 	}
 
 	function getMessage2($row) {
