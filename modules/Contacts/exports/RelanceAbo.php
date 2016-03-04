a0b1fb6755af2595768aa6075fe9a89d34153590
VERNEREY <aurelien.vernerey@sortirdunucleaire.fr>
2016-03-04 15:29:24 +0100

    add the export for the "relance abo "

---
+++ modules/Contacts/exports/RelanceAbo.php
@@ -0,0 +1,27 @@
+<?php
+/*+***********************************************************************************
+	AV1511
+ *************************************************************************************/
+
+class Contacts_RelanceAbo_Export extends Export_ExportData_Action { // TMP Fields !!!!!
+
+	//tmp check mailing ou other address !!!!!!!
+	function getExportStructure() {
+		return array(
+			"Ref" => function ($row) { return "Ref : " . $row["contact_no"]; },
+			"Ligne1" => function ($row) { return $row["firstname"] . " " . $row["lastname"]; },
+			"Ligne2" => function ($row) { return Contacts_ExportRevue_Export::getAddressField($row, "street2"); },
+			"Complément" => function ($row) { return Contacts_ExportRevue_Export::getAddressField($row, "street3"); },
+			"Adresse4" => function ($row) { return Contacts_ExportRevue_Export::getAddressField($row, "street"); },
+			"Adresse5" => function ($row) { return Contacts_ExportRevue_Export::getAddressField($row, "pobox"); },
+			"CodePostal" => function ($row) { return Contacts_ExportRevue_Export::getAddressField($row, "street"); },
+			"Ville" => function ($row) { return Contacts_ExportRevue_Export::getAddressField($row, "city"); },
+			"Pays" => function ($row) { return Contacts_ExportRevue_Export::getAddressField($row, "country"); },
+			"" => function ($row) { return $row["firstname"]; },
+		);
+	}
+	
+	function displayHeaderLine() {
+		return true;
+	}
+}
