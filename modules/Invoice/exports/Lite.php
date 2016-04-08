c9a79c00f8441fe2338f5be41f43b387e9359f0f
VERNEREY <aurelien.vernerey@sortirdunucleaire.fr>
2016-04-08 15:06:49 +0200

    add a lite export for invoice

---
+++ modules/Invoice/exports/Lite.php
@@ -0,0 +1,28 @@
+<?php
+/*+***********************************************************************************
+	AV1511
+ *************************************************************************************/
+
+class Invoice_Lite_Export extends Export_ExportData_Action {
+
+	// function getExportQuery(Vtiger_Request $request) {//depand of the export type...
+
+	// }
+
+	function getExportStructure() {
+		return array(
+			"Numero facture" => "invoice_no",
+			"Libellé" => "subject",
+			"Total" => "total",
+		);
+	}
+
+	function displayHeaderLine() {
+		return true;
+	}
+
+	function getExportFileName($request) {
+		$moduleName = $request->get('source_module');
+		return str_replace(' ','_',vtranslate($moduleName, $moduleName)) . "_Export_Test";
+	}
+}
