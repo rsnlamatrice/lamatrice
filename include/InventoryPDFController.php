866e2ef22b13823532cc7d87260da67acfe77d3d
rsnaurelien <aurelien.vernerey@sortirdunucleaire.org>
2016-04-20 12:41:03 +0200

    comment the total TVA line

--- include/InventoryPDFController.php
+++ include/InventoryPDFController.php
@@ -266,7 +266,7 @@ class Vtiger_InventoryPDFController {
 			$summaryModel->set(getTranslatedString("Shipping & Handling Tax:", $this->moduleName)." ($sh_tax_percent%)", $this->formatPrice($final_details['shtax_totalamount']));
 		if((float)$final_details['adjustment'])
 			$summaryModel->set(getTranslatedString("Adjustment", $this->moduleName), $this->formatPrice($final_details['adjustment']));
-		$summaryModel->set(getTranslatedString("Total TVA", $this->moduleName), $this->formatPrice($this->totaltaxes)); // TODO add currency string
+		//$summaryModel->set(getTranslatedString("Total TVA", $this->moduleName), $this->formatPrice($this->totaltaxes)); // TODO add currency string
 		$summaryModel->set(getTranslatedString("Total", $this->moduleName), $this->formatPrice($grandTotal)); // TODO add currency string
 
 		if ($this->moduleName == 'Invoice') {
