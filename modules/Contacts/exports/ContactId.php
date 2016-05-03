3208736507372ceddc05feee335624c7c9e2fbea
VERNEREY <aurelien.vernerey@sortirdunucleaire.fr>
2016-05-03 12:54:28 +0200

    add id export for contacts

--- modules/Contacts/exports/ContactId.php
+++ modules/Contacts/exports/ContactId.php
@@ -23,4 +23,18 @@ class Contacts_ContactId_Export extends Export_ExportData_Action {
 		$moduleName = $request->get('source_module');
 		return str_replace(' ','_',vtranslate($moduleName, $moduleName)) . "_id_export";
 	}
+
+	function getExportQuery($request) {//tmp ...
+		$parentQuery = parent::getExportQuery($request);
+
+		$fromPos = strpos($parentQuery, 'FROM');//tmp attention si il y a plusieurs clauses FROM
+		$wherePos = strpos($parentQuery, 'WHERE');//tmp attention si il y a plusieurs clauses WHERE
+		$query = substr($parentQuery, 0, $fromPos) . ", vtiger_contactdetails.contactid " .
+				 substr($parentQuery, $fromPos, ($wherePos - $fromPos)) .
+				 substr($parentQuery, $wherePos);
+
+		//echo '<br/><br/><br/>' . $query;
+
+		return $query;
+	}
 }
