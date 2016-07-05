b8503a3afe7fd2ad38b1f000aad4f960d36231bc
VERNEREY <aurelien.vernerey@sortirdunucleaire.fr>
2016-07-05 16:48:45 +0200

    add module statistique filter

---
+++ languages/en_us/RSNFiltreStatistique.php
@@ -0,0 +1,23 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$languageStrings = array(
+	'RSNFiltreStatistique' => 'Stat Filters',
+	'SINGLE_RSNFiltreStatistique' => 'Stat Filter',
+	'LBL_BLOCK_GENERAL_INFORMATION' => '',
+	'LBL_BLOCK_SYSTEM_INFORMATION' => '',
+	'LBL_NAME' => 'name',
+	'LBL_FILTERVALUEQUERY' => 'Filter Value Query',
+	'LBL_FILTERNAMEFIELDNAME' => 'Name of the filter name field',
+	'STATISTIQUE' => 'statistic',
+
+);
+
+?>
