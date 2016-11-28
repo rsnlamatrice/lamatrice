8e9a6d34dbc13b63dc18f4fd75a8ab50447ca253
VERNEREY <aurelien.vernerey@sortirdunucleaire.fr>
2016-11-28 16:33:40 +0100

    add a new export for web mailing

---
+++ modules/Contacts/exports/ExportWebMailing.php
@@ -0,0 +1,173 @@
+<?php
+/*+***********************************************************************************
+	AV1511
+ *************************************************************************************/
+
+class Contacts_ExportWebMailing_Export extends Export_ExportData_Action {
+
+	//tmp mailing address ?? ...
+	function getExportStructure() {
+		return array(
+			"ref" => "contact_no",
+			"email" => "export_email"
+		);
+	}
+
+	function getAutresAnneeAdhesion($row) {
+		$adhs = explode(";", $row["adh_list"]);
+		$adh_size = sizeof($adhs);
+
+		if ($adh_size > 0 && $adhs[0] != "") {
+			rsort($adhs);
+			$current_year = date("Y");//tmp dernière adhésion et non année en cours ? -> utiliser la date de l'AG !! -> ne pas afficher les adhésion pour l'année après AG (pas afficher ADH16 avant ag 16)
+			$curent_adh = false;
+			$precedement_adhs = [];
+
+			for ($i = 0; $i < $adh_size; ++$i) {
+				$year = intval(str_replace("ADH", "20", $adhs[$i]));
+
+				if ($year == $current_year) {
+					$curent_adh = true;
+				} else {
+					array_push($precedement_adhs, $year);
+				}
+			}
+
+			$return_value = "";
+
+			if ($curent_adh) {
+				$return_value .= "Adhérent " . $current_year . " ";
+			}
+			if (sizeof($precedement_adhs) > 0) {
+				$return_value .= ($curent_adh) ? "( et " : "( précédemment en ";
+				for ($i = 0; $i < sizeof($precedement_adhs); ++$i) {
+					$return_value .= $precedement_adhs[$i] . " ";
+				}
+				$return_value .= ")";
+			}
+
+			return $return_value;
+		}
+
+		return "";
+	}
+	
+	function displayHeaderLine() {
+		return true;
+	}
+
+	function getNbAdhenrents($row) {
+		return ($row["grpnbremembres"] > 0) ? $row["grpnbremembres"] : "";
+	}
+
+	function getWebSite($row) {
+		return (strlen($row["websiteurl"]) < 3 || strrpos($row["websiteurl"], "http") === 0) ? $row["websiteurl"] : "http://" . $row["websiteurl"];
+	}
+
+	function getExportFileName($request) {
+		$moduleName = $request->get('source_module');
+		return "liste_groupes_pour_stats";
+	}
+
+	//tmp order by zip ??
+	function getQueryOrderBy($moduleName) {
+		return ' ORDER BY mailingcountry ASC, mailingzip ASC';
+	}
+
+	function getMainEmail($row) {
+		return ($row['emailoptout']) ? "" : $row['email'];
+	}
+
+	function getExportQuery($request) {//tmp ...
+		$parentQuery = parent::getExportQuery($request);
+
+		$fromPos = strpos($parentQuery, 'FROM');//tmp attention si il y a plusieurs clauses FROM
+		$wherePos = strpos($parentQuery, 'WHERE');//tmp attention si il y a plusieurs clauses WHERE
+		$query = substr($parentQuery, 0, $fromPos) . ", export_emails.email as export_email " .
+				 substr($parentQuery, $fromPos, ($wherePos - $fromPos)) . 
+					 "JOIN ( SELECT contactid, email FROM 
+					 (
+					/* adresse email principale dans rézo-info
+					13 039 */
+					SELECT `vtiger_contactemails`.contactid, `vtiger_contactemails`.email
+					FROM `vtiger_contactemails`
+					JOIN `vtiger_contactdetails`
+					    ON `vtiger_contactemails`.contactid = `vtiger_contactdetails`.contactid
+					JOIN vtiger_crmentity vtiger_crmentity_contacts
+					    ON vtiger_crmentity_contacts.crmid = vtiger_contactdetails.contactid
+					JOIN vtiger_crmentity vtiger_crmentity_emails
+					    ON vtiger_crmentity_emails.crmid = vtiger_contactemails.`contactemailsid`
+					 WHERE  vtiger_crmentity_contacts.deleted = 0
+					AND vtiger_crmentity_emails.deleted = 0
+					AND `vtiger_contactemails`.emailoptout = 0
+					AND `vtiger_contactdetails`.emailoptout = 0
+					AND `vtiger_contactemails`.`rsnmediadocuments` LIKE '%rezo-info%'
+					AND `vtiger_contactemails`.`emailaddressorigin` = 'Principale'
+
+
+					UNION
+
+					/* adresse email secondaire dans rézo-info
+					2 440 */
+					SELECT  `vtiger_contactemails`.contactid, `vtiger_contactemails`.email
+					FROM `vtiger_contactemails`
+					JOIN `vtiger_contactdetails`
+					    ON `vtiger_contactemails`.contactid = `vtiger_contactdetails`.contactid
+					JOIN vtiger_crmentity vtiger_crmentity_contacts
+					    ON vtiger_crmentity_contacts.crmid = vtiger_contactdetails.contactid
+					JOIN vtiger_crmentity vtiger_crmentity_emails
+					    ON vtiger_crmentity_emails.crmid = vtiger_contactemails.`contactemailsid`
+					WHERE `vtiger_contactemails`.`emailaddressorigin` != 'Principale'
+					AND vtiger_crmentity_contacts.deleted = 0
+					AND vtiger_crmentity_emails.deleted = 0
+					AND `vtiger_contactemails`.emailoptout = 0
+					AND `vtiger_contactdetails`.emailoptout = 0
+					AND `vtiger_contactemails`.`rsnmediadocuments` LIKE '%rezo-info%'
+
+
+
+					UNION
+
+					/* adresse email principale de ceux qui ne n'ont pas d'adresse secondaire dans rézo-info
+					85  666 */
+					SELECT  `vtiger_contactemails`.contactid, `vtiger_contactemails`.email
+					FROM `vtiger_contactemails`
+					JOIN `vtiger_contactdetails`
+					    ON `vtiger_contactemails`.contactid = `vtiger_contactdetails`.contactid
+					JOIN vtiger_crmentity vtiger_crmentity_contacts
+					    ON vtiger_crmentity_contacts.crmid = vtiger_contactdetails.contactid
+					JOIN vtiger_crmentity vtiger_crmentity_emails
+					    ON vtiger_crmentity_emails.crmid = vtiger_contactemails.`contactemailsid`
+					 WHERE `vtiger_contactemails`.`emailaddressorigin` = 'Principale'
+					AND vtiger_crmentity_contacts.deleted = 0
+					AND vtiger_crmentity_emails.deleted = 0
+					AND `vtiger_contactemails`.emailoptout = 0
+					AND `vtiger_contactdetails`.emailoptout = 0
+					AND (NOT `vtiger_contactemails`.`rsnmediadocuments` LIKE '%rezo-info%')
+					AND  `vtiger_contactemails`.contactid NOT IN (
+
+					    /* adresse email secondaire dans rézo-info */
+					    SELECT  `vtiger_contactemails`.contactid
+					    FROM `vtiger_contactemails`
+					    JOIN `vtiger_contactdetails`
+					        ON `vtiger_contactemails`.contactid = `vtiger_contactdetails`.contactid
+					    JOIN vtiger_crmentity vtiger_crmentity_contacts
+					        ON vtiger_crmentity_contacts.crmid = vtiger_contactdetails.contactid
+					    JOIN vtiger_crmentity vtiger_crmentity_emails
+					        ON vtiger_crmentity_emails.crmid = vtiger_contactemails.`contactemailsid`
+					    WHERE `vtiger_contactemails`.`emailaddressorigin` != 'Principale'
+					    AND vtiger_crmentity_contacts.deleted = 0
+					    AND vtiger_crmentity_emails.deleted = 0
+						AND `vtiger_contactemails`.emailoptout = 0
+						AND `vtiger_contactdetails`.emailoptout = 0
+					    AND `vtiger_contactemails`.`rsnmediadocuments` LIKE '%rezo-info%'
+
+					)) AS all_emails /*GROUP BY contactid*/) AS export_emails ON export_emails.contactid = vtiger_contactdetails.contactid
+					" .
+				 substr($parentQuery, $wherePos);
+
+		//echo '<br/><br/><br/>' . $query;
+
+		return $query;
+	}
+}
