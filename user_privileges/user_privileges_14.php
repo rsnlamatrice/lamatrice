28858d06d874e35c7790f880c9368192cedb99d8
Bastien Ho <bastienho@eelv.fr>
2017-05-23 10:54:59 +0200

    Cleanup repo

--- user_privileges/user_privileges_14.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-//This is the access privilege file
-$is_admin=true;
-
-$user_info=array('user_name'=>'Charlotte','is_admin'=>'on','user_password'=>'$1$Ch000000$4EYADK36VM4WGpJrhdCAE/','confirm_password'=>'$1$Ch000000$4EYADK36VM4WGpJrhdCAE/','first_name'=>'Charlotte','last_name'=>'M','roleid'=>'H11','email1'=>'charlotte.mijeon@sortirdunucleaire.fr','status'=>'Active','activity_view'=>'This Week','lead_view'=>'Today','hour_format'=>'24','end_hour'=>'','start_hour'=>'07:00','title'=>'Charg&eacute;e de presse','phone_work'=>'','department'=>'','phone_mobile'=>'','reports_to_id'=>'','phone_other'=>'','email2'=>'','phone_fax'=>'','secondaryemail'=>'','phone_home'=>'','date_format'=>'dd-mm-yyyy','signature'=>'','description'=>'','address_street'=>'','address_city'=>'','address_state'=>'','address_postalcode'=>'','address_country'=>'','accesskey'=>'IZdbNm54j10p3sg','time_zone'=>'Europe/Brussels','currency_id'=>'1','currency_grouping_pattern'=>'123,456,789','currency_decimal_separator'=>'.','currency_grouping_separator'=>'&#039;','currency_symbol_placement'=>'1.0$','imagename'=>'','internal_mailer'=>'0','theme'=>'woodspice','language'=>'fr_fr','reminder_interval'=>'1 Day','no_of_currency_decimals'=>'4','truncate_trailing_zeros'=>'1','dayoftheweek'=>'Monday','callduration'=>'10','othereventduration'=>'60','calendarsharedtype'=>'public','default_record_view'=>'Summary','leftpanelhide'=>'1','rowheight'=>'medium','default_module'=>'RSNMediaRelations','ccurrency_name'=>'','currency_code'=>'EUR','currency_symbol'=>'&#8364;','conv_rate'=>'1.00000','record_id'=>'','record_module'=>'','currency_name'=>'Euro','id'=>'14');
-
-?>
--- user_privileges/user_privileges_5.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-//This is the access privilege file
-$is_admin=true;
-
-$user_info=array('user_name'=>'Baptiste','is_admin'=>'on','user_password'=>'$1$Ba000000$wdVdHil/yPNDAcrP8liOB0','confirm_password'=>'$1$Ba000000$wdVdHil/yPNDAcrP8liOB0','first_name'=>'Baptiste','last_name'=>'C','roleid'=>'H6','email1'=>'baptiste.coll@sortirdunucleaire.org','status'=>'Active','activity_view'=>'Today','lead_view'=>'Today','hour_format'=>'24','end_hour'=>'','start_hour'=>'07:00','title'=>'','phone_work'=>'','department'=>'','phone_mobile'=>'','reports_to_id'=>'','phone_other'=>'','email2'=>'','phone_fax'=>'','secondaryemail'=>'','phone_home'=>'','date_format'=>'dd-mm-yyyy','signature'=>'','description'=>'','address_street'=>'','address_city'=>'','address_state'=>'National','address_postalcode'=>'','address_country'=>'','accesskey'=>'0rcM2m9IkydB2gir','time_zone'=>'Europe/Amsterdam','currency_id'=>'1','currency_grouping_pattern'=>'123,456,789','currency_decimal_separator'=>',','currency_grouping_separator'=>'&#039;','currency_symbol_placement'=>'1.0$','imagename'=>'_DSC2302.JPG','internal_mailer'=>'0','theme'=>'nature','language'=>'fr_fr','reminder_interval'=>'','no_of_currency_decimals'=>'4','truncate_trailing_zeros'=>'1','dayoftheweek'=>'Monday','callduration'=>'10','othereventduration'=>'120','calendarsharedtype'=>'public','default_record_view'=>'Summary','leftpanelhide'=>'0','rowheight'=>'narrow','default_module'=>'Invoice','ccurrency_name'=>'','currency_code'=>'EUR','currency_symbol'=>'&#8364;','conv_rate'=>'1.00000','record_id'=>'','record_module'=>'','currency_name'=>'Euro','id'=>'5');
-
-?>
