b4d6cf0edf6c5a4b3a94da298c8d46ad84cdf1ba
bubu <bubu@riseup.net>
2022-08-11 14:14:03 +0200

    Ajout informations résumé lorsque l'on sélectionne les factures
    Ajout Total produit, total don et total service pour le module facture.

--- modules/Invoice/views/ListAjax.php
+++ modules/Invoice/views/ListAjax.php
@@ -20,8 +20,14 @@ class Invoice_ListAjax_View extends Vtiger_ListAjax_View {
 		$result['viewname'] = $cvId;
 		
 		//ED150904 : complete le nombre par le cumul ttc
-		if($calculatedTotals['total'])
-			$result['count'] = $count . ' : ' . CurrencyField::convertToUserFormat($calculatedTotals['total']) . ' €'; //TODO $USER_MODEL->get('currency_symbol')
+		if($calculatedTotals['total']){
+			$result['count'] = $count . ' : '; //TODO $USER_MODEL->get('currency_symbol')
+			$result['count'] .= 'Total TTC : '.CurrencyField::convertToUserFormat($calculatedTotals['total']) . ' €; ';
+			$result['count'] .= 'Total Produit : '.CurrencyField::convertToUserFormat($calculatedTotals['total_product']) . ' €; ';
+			$result['count'] .= 'Total Don : '.CurrencyField::convertToUserFormat($calculatedTotals['total_don']) . ' €; ';
+			$result['count'] .= 'Total Service : '.CurrencyField::convertToUserFormat($calculatedTotals['total_service']) . ' €; ';
+		}
+			
 		else
 			$result['count'] = $count;
 
