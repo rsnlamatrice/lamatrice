d716d5e9f0097254f782a04ad830a4b2dcc3b45f
bubu <bubu@riseup.net>
2022-08-12 09:59:39 +0200

    Modification mise en forme des informations résumées
    - ajout de | au lieu de ;
    - modification de l'ordre

--- modules/Invoice/views/ListAjax.php
+++ modules/Invoice/views/ListAjax.php
@@ -22,10 +22,10 @@ class Invoice_ListAjax_View extends Vtiger_ListAjax_View {
 		//ED150904 : complete le nombre par le cumul ttc
 		if($calculatedTotals['total']){
 			$result['count'] = $count . ' : '; //TODO $USER_MODEL->get('currency_symbol')
-			$result['count'] .= 'Total TTC : '.CurrencyField::convertToUserFormat($calculatedTotals['total']) . ' €; ';
-			$result['count'] .= 'Total Produit : '.CurrencyField::convertToUserFormat($calculatedTotals['total_product']) . ' €; ';
-			$result['count'] .= 'Total Don : '.CurrencyField::convertToUserFormat($calculatedTotals['total_don']) . ' €; ';
-			$result['count'] .= 'Total Service : '.CurrencyField::convertToUserFormat($calculatedTotals['total_service']) . ' €; ';
+			$result['count'] .= 'Total TTC : '.CurrencyField::convertToUserFormat($calculatedTotals['total']) . ' € | ';
+			$result['count'] .= 'Total Produit : '.CurrencyField::convertToUserFormat($calculatedTotals['total_product']) . ' € | ';
+			$result['count'] .= 'Total Service : '.CurrencyField::convertToUserFormat($calculatedTotals['total_service']) . ' € | ';
+			$result['count'] .= 'Total Don : '.CurrencyField::convertToUserFormat($calculatedTotals['total_don']) . ' €';
 		}
 			
 		else
