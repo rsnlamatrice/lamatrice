1408dcb9a7b7c2f49ea0ebc297c54ed02bac0435
bubu <bubu@riseup.net>
2023-04-17 16:29:12 +0200

    Ajout relation avec RSNMedias pour l'utilisation des panel RSNMedias dans RSNMediaContacts

--- modules/RSNMediaContacts/models/Relation.php
+++ modules/RSNMediaContacts/models/Relation.php
@@ -5,17 +5,28 @@
 
 class RSNMediaContacts_Relation_Model extends Vtiger_Relation_Model{
 
-	// La suppression de la relation entre tables conduit la suppression de l'entité
-	public function deleteRelation($sourceRecordId, $relatedRecordId){
-		if(parent::deleteRelation($sourceRecordId, $relatedRecordId)){
-			
-			$record = Vtiger_Record_Model::getInstanceById($relatedRecordId, 'RSNMediaRelations');
-			if($record->getModule() == 'RSNMediaRelations')
-				$record->delete();
-			
-			return true;
-		}
-		else
-			return false;
-	}
+    // La suppression de la relation entre tables conduit la suppression de l'entitï¿½ï¿½
+    public function deleteRelation($sourceRecordId, $relatedRecordId){
+        if(parent::deleteRelation($sourceRecordId, $relatedRecordId)){
+            
+            $record = Vtiger_Record_Model::getInstanceById($relatedRecordId, 'RSNMediaRelations');
+            if($record->getModule() == 'RSNMediaRelations')
+                $record->delete();
+            
+            return true;
+        }
+        else
+            return false;
+    }
+
+
+    public function getModulesInfoForDetailView() {
+        return array(
+            'RSNMedias' => array(
+                'fieldName' => 'rsnmediasid', 
+                'tableName' => 'vtiger_rsnmedias',
+                'sourceFieldName' => 'vtiger_rsnmediacontacts.rsnmediaid',
+            ),
+        );
+    }
 }
